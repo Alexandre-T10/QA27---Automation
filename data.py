9d81892ab7bc9f0f0d4105b1d4f377b589fe65a6
Alexandre Silva <alexix.dasilva@gmail.com>
2026-04-21 09:57:31 -0300

    data.py

--- data.py
+++ data.py
@@ -1,2 +1,7 @@
 URBAN_ROUTES_URL = 'https://cnt-1fb301d9-6361-470a-a94c-99af1d1b2bc2.containerhub.tripleten-services.com?lng=pt'
 ADDRESS_FROM = 	'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = "Pare no bar de sucos"
